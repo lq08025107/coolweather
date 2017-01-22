cf621adbe928fe1e5ba7304b7c935399c6bb6c96
lq08025107 <534525962@qq.com>
2017-01-22 10:44:04 +0800

    First commit

---
+++ app/src/main/java/iliuqiang/net/coolweather/db/City.java
@@ -0,0 +1,49 @@
+package iliuqiang.net.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by LQ on 2017/1/22.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+
+    }
+
+    private int provinceId;
+
+}
